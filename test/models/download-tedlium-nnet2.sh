7a6595fdc9164813a0d20f374428544ede40f5f9
Marco D'Agostini <madacol10@gmail.com>
2018-02-13 13:32:26 -0500

    Resumes download

--- test/models/download-tedlium-nnet2.sh
+++ test/models/download-tedlium-nnet2.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-curl https://phon.ioc.ee/~tanela/tedlium_nnet_ms_sp_online.tgz | tar zxv
+BASE_URL="https://phon.ioc.ee/~tanela/tedlium_nnet_ms_sp_online.tgz"
+TMP_FILE="/tmp/nnet2_model.tgz"
+
+curl -C - -o $TMP_FILE $BASE_URL
+tar -zxvf $TMP_FILE
